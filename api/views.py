0f7a060f69c76fe300ff91100eaba0b933af07cd
Manoeljr <manoelv8@gmail.com>
2022-03-15 17:28:35 -0300

    Adicionando a View

--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from api.api.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        '/api/tokens/',
+        '/api/register/',
+        '/api/token/refresh/'
+    ]
+    return Response(routes)
